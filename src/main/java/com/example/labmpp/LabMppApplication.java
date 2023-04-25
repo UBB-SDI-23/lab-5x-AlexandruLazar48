b430ddfb813b57eba8029b09b80f3cf3ae6c1c6e
Alexandru Lazar <alexandru.lazar.ext@siemens.com>
2023-04-25 18:01:06 +0300

    port 80

--- src/main/java/com/example/labmpp/LabMppApplication.java
+++ src/main/java/com/example/labmpp/LabMppApplication.java
@@ -28,7 +28,7 @@ public class LabMppApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost", "http://http://35.228.57.4"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost", "http://35.228.57.4"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
